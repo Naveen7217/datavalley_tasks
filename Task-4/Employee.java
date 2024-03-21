b838dff5bdfe061238dacf69b7eb0167049cce07
Naveen7217 <85105602+Naveen7217@users.noreply.github.com>
2024-03-21 16:09:38 +0530

    Add files via upload

---
+++ Task-4/Employee.java
@@ -0,0 +1,72 @@
+import java.util.LinkedList;
+import java.util.Comparator;
+
+class Employee {
+    private int id;
+    private String name;
+    private String designation;
+    private double salary;
+
+    public Employee(int id, String name, String designation, double salary) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", designation='" + designation + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
+public class Employee{
+    public static void main(String[] args) {
+		
+        LinkedList<Employee> employeeList = new LinkedList<>();
+
+
+        employeeList.add(new Employee(101, "John", "Manager", 80000.0));
+        employeeList.add(new Employee(102, "Alice", "Developer", 65000.0));
+        employeeList.add(new Employee(103, "Bob", "Tester", 60000.0));
+        employeeList.add(new Employee(104, "Eva", "Designer", 70000.0));
+
+
+        employeeList.sort(Comparator.comparing(Employee::getName));
+
+
+        System.out.println("Sorted by name:");
+        for (Employee emp : employeeList) {
+            System.out.println(emp);
+        }
+
+
+        employeeList.sort(Comparator.comparingDouble(Employee::getSalary));
+
+        System.out.println("\nSorted by salary:");
+        for (Employee emp : employeeList) {
+            System.out.println(emp);
+        }
+    }
+}
